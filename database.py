5a3b33f9dff0bd480f17d4ca09accb55d4270852
Rob McMullen <robm@users.sourceforge.net>
2011-11-29 14:15:01 -0800

    Added subclasses of SortableGuess to represent media types * added hierarchical creation * added new test_hierarchy unit test

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import bisect
+
 from guessit.guess import Guess
 
 class Database(object):
@@ -12,46 +14,232 @@ class Database(object):
 
 
 class SortableGuess(Guess):
+    @classmethod
+    def convertGuess(cls, g):
+        guess = None
+        if g['type'] == 'movie':
+            guess = Movie(g)
+        elif g['type'] == 'episode':
+            if 'episodeNumber' in g:
+                guess = SeriesEpisode(g)
+            elif 'extraNumber' in g:
+                guess = SeriesBonus(g)
+        if guess is None:
+            raise RuntimeError("Unsupported guess type %s" % g['type'])
+        return guess
+    
     def __init__(self, other):
         Guess.__init__(self)
-        dict.update(self, other)
-        for prop in other:
-            if prop in other._confidence:
-                self._confidence[prop] = other._confidence[prop]
-            else:
-                self._confidence[prop] = -1.0
+        ignore = self.filter_out()
+        for k in other:
+            if k not in ignore:
+                self[k] = other[k]
+                if k in other._confidence:
+                    self._confidence[k] = other._confidence[k]
+                else:
+                    self._confidence[k] = -1.0
+        self.children = []
+        
+        self.normalize()
+    
+    def filter_out(self):
+        return set()
+    
+    def normalize(self):
+        self.add_missing_entries()
+        self.calc_group_key()
+        self.canonicalize()
+    
+    def __str__(self):
+        return self.canonical_title
 
     def __cmp__(self, other):
         return cmp(self.decorate(), other.decorate())
 
+    def add_missing_entries(self):
+        if 'title' not in self:
+            title, _ = os.path.splitext(os.path.basename(self['pathname']))
+            self['title'] = title
+    
+    def calc_group_key(self):
+        self.group_key = self['type'], self['title']
+            
+    def canonicalize(self):
+        self.in_context_title = self['title']
+        self.canonical_title = self['title']
+
     def decorate(self):
-        if self['type'] == 'movie':
-            entry = (self.get('title', ""),
-                     9999,
-                     self.get('extraNumber', 9999),
-                     self.get('extraTitle', ""),
-                     )
-        elif self['type'] == 'episode':
-            entry = (self.get('series', ""),
-                     self.get('episodeNumber', 9999),
-                     self.get('extraNumber', 9999),
-                     self.get('extraTitle', ""),
-                     )
-        else:
-            entry = (self.get('title', ""),
-                     9999,
-                     9999,
-                     "",
-                     )
+        entry = (self.get('title', ""),
+                 9999,
+                 9999,
+                 9999,
+                 "",
+                 )
         return entry
     
+    def decompose(self):
+        raise RuntimeError
+
+    def add_chain(self, chain):
+        if chain:
+            node = chain[0]
+            pos = bisect.bisect_left(self.children, node)
+            if pos != len(self.children) and self.children[pos].canonical_title == node.canonical_title:
+                #print "Found %s" % node
+                pass
+            else:
+                #print "Inserting %s" % node
+                bisect.insort_left(self.children, node)
+            parent = self.children[pos]
+            parent.add_chain(chain[1:])
+
+    def str_hierarchy(self):
+        text = []
+        text.append(str(self))
+        for node in self.children:
+            text.append(node.str_hierarchy())
+        return "\n".join(text)
+
+class Root(SortableGuess):
+    def __init__(self, title):
+        other = Guess(title=title, type='root')
+        SortableGuess.__init__(self, other)
+
+class Movie(SortableGuess):
+    def add_missing_entries(self):
+        if 'title' not in self:
+            title, _ = os.path.splitext(os.path.basename(self['pathname']))
+            self['title'] = title
+    
+    def decorate(self):
+        entry = (self.get('title', ""),
+                 9999,
+                 9999,
+                 self.get('extraNumber', 9999),
+                 self.get('extraTitle', ""),
+                 )
+        return entry
+    
+    def decompose(self):
+        return [MovieTitle(self), self]
+
+class MovieTitle(Movie):
+    def decorate(self):
+        entry = (self.get('title', ""),
+                 9999,
+                 9999,
+                 9999,
+                 "",
+                 )
+        return entry
+    
+    def decompose(self):
+        return [self]
+
+class MovieSeries(Movie):
+    pass
+
+class SeriesEpisode(SortableGuess):
+    def decorate(self):
+        entry = (self.get('series', ""),
+                 self.get('season', 9999),
+                 self.get('episodeNumber', 9999),
+                 self.get('extraNumber', 9999),
+                 self.get('extraTitle', ""),
+                 )
+        return entry
+    
+    def calc_group_key(self):
+        self.group_key = self['type'], self['series']
+            
+    def add_missing_entries(self):
+        if 'series' not in self:
+            title, _ = os.path.splitext(os.path.basename(self['pathname']))
+            self['series'] = title
+            
+    def canonicalize(self):
+        self.in_context_title = "Episode %d %s" % (self['episodeNumber'], self.get('episodeTitle',""))
+        self.canonical_title = "%s Season %d %s" % (self['series'], self['season'], self.in_context_title)
+    
+    def decompose(self):
+        return [Series(self), Season(self), self]
+
+class SeriesBonus(SeriesEpisode):
+    def canonicalize(self):
+        self.in_context_title = "Bonus Feature %d %s" % (self['extraNumber'], self.get('extraTitle',""))
+        self.canonical_title = "%s Season %d %s" % (self['series'], self['season'], self.in_context_title)
+    
+    def decompose(self):
+        return [Series(self), Season(self), self]
+
+class Series(SeriesEpisode):
+    def filter_out(self):
+        return set(['pathname', 'season', 'episodeNumber', 'title', 'extraNumber', 'extraTitle'])
+    
+    def decorate(self):
+        entry = (self.get('series', ""),
+                 9999,
+                 9999,
+                 9999,
+                 "",
+                 )
+        return entry
+    
+    def canonicalize(self):
+        self.canonical_title = self['series']
+        self.in_context_title = self.canonical_title
+    
+    def decompose(self):
+        return [self]
+
+class Season(SeriesEpisode):
+    def filter_out(self):
+        return set(['pathname', 'episodeNumber', 'title', 'extraNumber', 'extraTitle'])
+    
+    def decorate(self):
+        entry = (self.get('series', ""),
+                 self.get('season', 9999),
+                 9999,
+                 9999,
+                 "",
+                 )
+        return entry
+    
+    def canonicalize(self):
+        self.in_context_title = "Season %d" % self['season']
+        self.canonical_title = "%s %s" % (self['series'], self.in_context_title)
+    
+    def decompose(self):
+        return [Series(self), self]
+
+
+class GuessResults(list):
+    def __str__(self):
+        text = [str(s) for s in self]
+        return "\n".join(text)
+    
+    def hierarchy(self):
+        name_groups = {}
+        for guess in self:
+            t = guess.group_key
+            if t not in name_groups:
+                name_groups[t] = []
+            name_groups[t].append(guess)
+        h = Root("root")
+        for guesses in name_groups.itervalues():
+            for child in guesses:
+                chain = child.decompose()
+                h.add_chain(chain)
+        #print h.str_hierarchy()
+        return h.children
+
 
 class DictDatabase(Database):
     def create(self):
         self.cats = {}
     
     def add(self, g):
-        guess = SortableGuess(g)
+        guess = SortableGuess.convertGuess(g)
         category = guess['type']
         if category is not None:
             if category in self.aliases:
@@ -60,19 +248,17 @@ class DictDatabase(Database):
                 self.cats[category] = {}
             
             self.cats[category][guess['pathname']] = guess
-            print "added: %s" % guess.nice_string()
     
     def find(self, category, criteria=None):
         if category not in self.cats:
             return []
         cat = self.cats[category]
-        results = []
+        results = GuessResults()
         if criteria is None:
             criteria = lambda s: True
         for item in cat.itervalues():
             valid = criteria(item)
             if valid:
                 results.append(item)
-        #results = self.sort(results)
         results.sort()
         return results
---
+++ test/test_hierarchy.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# GuessIt - A library for guessing information from filenames
+# Copyright (c) 2011 Nicolas Wack <wackou@gmail.com>
+#
+# GuessIt is free software; you can redistribute it and/or modify it under
+# the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# GuessIt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+from guessittest import *
+
+from media import guess_custom
+from config import Config, RootMenu
+from database import DictDatabase
+
+class TestDatabaseHierarchyMovies1(TestCase):
+    def setUp(self):
+        self.config = Config([])
+        self.db = DictDatabase()
+        self.root = RootMenu(self.db)
+        self.config.parse_dir(self.root, "movies1")
+        
+    def testFind(self):
+        results = self.db.find("movie")
+        assert len(results) == 38
+        hier = results.hierarchy()
+        self.assertEqual(len(hier), 15)
+    
+    def testFindChildren1(self):
+        results = self.db.find("movie", lambda s:s['title'] == "Moon")
+        hier = results.hierarchy()
+        self.assertEqual(len(hier), 1)
+        self.assertEqual(len(hier[0].children), 5)
+        self.assertEqual(hier[0].children[0]['title'], "Moon")
+        self.assertEqual(hier[0].children[0]['extraNumber'], 1)
+        
+class TestDatabaseHierarchySeries1(TestCase):
+    def setUp(self):
+        self.config = Config([])
+        self.db = DictDatabase()
+        self.root = RootMenu(self.db)
+        self.config.parse_dir(self.root, "series1")
+        
+    def testFind(self):
+        results = self.db.find("episode")
+        self.assertEqual(len(results), 32)
+        h = results.hierarchy()
+        print h
+        self.assertEqual(len(h), 5)
+    
+    def testFindChildren1(self):
+        results = self.db.find("episode", lambda s:s['series'] == "The Big Bang Theory")
+        self.assertEqual(len(results), 22)
+        h = results.hierarchy()
+        print h
+        self.assertEqual(len(h), 1)
+        
+        # Look at the series
+        series = h[0]
+        self.assertEqual(series['series'], "The Big Bang Theory")
+        
+        # Look at the second season
+        season2 = series.children[1]
+        self.assertEqual(season2['series'], "The Big Bang Theory")
+        c = season2.children
+        self.assertEqual(len(c), 6)
+        self.assertEqual(c[0]['series'], "The Big Bang Theory")
+        self.assertEqual(c[0]['season'], 2)
+        self.assertEqual(c[0]['episodeNumber'], 1)
+        self.assertEqual(c[1]['series'], "The Big Bang Theory")
+        self.assertEqual(c[1]['season'], 2)
+        self.assertEqual(c[1]['episodeNumber'], 2)
+        self.assertEqual(c[5]['series'], "The Big Bang Theory")
+        self.assertEqual(c[5]['season'], 2)
+        self.assertEqual(c[5]['extraNumber'], 3)
+
+
+if __name__ == '__main__':
+    for case in [TestDatabaseHierarchyMovies1, TestDatabaseHierarchySeries1]:
+        suite = unittest.TestLoader().loadTestsFromTestCase(case)
+        TextTestRunner(verbosity=2).run(suite)
