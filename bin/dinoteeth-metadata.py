6325b7d464ebb70a503a0fcb02c0a4829fb469fe
Rob McMullen <robm@users.sourceforge.net>
2011-12-20 13:57:43 -0800

    Made display of database the default action * created '-f' option to trigger imdb/tmdb lookup

--- bin/dinoteeth-metadata.py
+++ bin/dinoteeth-metadata.py
@@ -29,12 +29,12 @@ if __name__ == "__main__":
     parser.add_option("-v", action="store_true", dest="verbose", default=False)
     parser.add_option("-o", action="store", dest="output", default="index.html")
     parser.add_option("--batch-metadata-database", action="store", dest="bdb", default="dinoteeth.bdb")
-    parser.add_option("-m", "--metadata-database", action="store", dest="mdb", default="dinoteeth.umdb")
+    parser.add_option("-m", "--metadata-database", action="store", dest="umdb", default="dinoteeth.umdb")
     parser.add_option("-d", "--database", action="store", dest="database", default="dinoteeth.db")
     parser.add_option("-i", "--image-dir", action="store", dest="image_dir", default="test/posters")
     parser.add_option("-r", "--regenerate", action="store_true", dest="regenerate", default=False)
     parser.add_option("-p", "--posters", action="store_true", dest="posters", default=False)
-    parser.add_option("-l", "--list", action="store_true", dest="list", default=False)
+    parser.add_option("-f", "--fetch", action="store_true", dest="fetch", default=False)
     (options, args) = parser.parse_args()
     print options
 
@@ -43,44 +43,47 @@ if __name__ == "__main__":
     c.options = options
     db = c.get_database()
     print db
-    if options.list:
-        results = db.find("movie")
-        for i, movie in enumerate(results):
-            print "%d: %s" % (i, movie)
-            print "  group key: %s" % str(movie.group_key)
-            print "  metadata: %s" % str(movie.metadata)
-            print
-        sys.exit()
         
-    bdb = MovieMetadataDatabase()
-    bdb.loadStateFromFile(options.bdb)
-    mdb = UnifiedMetadataDatabase()
-    mdb.loadStateFromFile(options.mdb)
-    
     if args:
         for path in args:
             c.parse_dir(db, path)
         db.saveStateToFile()
     
+    umdb = UnifiedMetadataDatabase()
+    umdb.loadStateFromFile(options.umdb)
+    
     if options.regenerate:
         results = db.find("movie")
         for i, movie in enumerate(results):
             print "%d: %s" % (i, movie)
-            mdb.regenerate(movie, bdb)
-        mdb.saveStateToFile()
+            umdb.regenerate(movie, bdb)
+        umdb.saveStateToFile()
         db.saveStateToFile()
     elif options.posters:
+        bdb = MovieMetadataDatabase()
+        bdb.loadStateFromFile(options.bdb)
         if not os.path.exists(options.image_dir):
             os.mkdir(options.image_dir)
         results = db.find("movie")
         for i, movie in enumerate(results):
             print "%d: %s" % (i, movie)
             bdb.fetch_poster(movie, options.image_dir)
+    elif options.fetch:
+        bdb = MovieMetadataDatabase()
+        bdb.loadStateFromFile(options.bdb)
+        results = db.find("movie")
+        for i, movie in enumerate(results):
+            print "%d: %s" % (i, movie)
+            umdb.add_info(movie, bdb)
+        umdb.saveStateToFile()
+        bdb.saveStateToFile()
+        db.saveStateToFile()
     else:
         results = db.find("movie")
         for i, movie in enumerate(results):
             print "%d: %s" % (i, movie)
-            mdb.add_info(movie, bdb)
-        mdb.saveStateToFile()
-        bdb.saveStateToFile()
-        db.saveStateToFile()
+            print "  group key: %s" % str(movie.group_key)
+            print "  metadata: %s" % str(movie.metadata)
+            print "  scanned: %s" % str(movie.scanned_metadata)
+            print
+        sys.exit()
