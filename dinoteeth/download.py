4e688de45e698221dd12b906209e978efa50fa0c
Rob McMullen <robm@users.sourceforge.net>
2012-10-17 16:32:18 -0700

    Added new thread and process download managers * added asyncronous http downloader

---
+++ dinoteeth/download.py
@@ -0,0 +1,178 @@
+import os, time, logging, threading, Queue
+import asyncore, socket, urlparse
+from cStringIO import StringIO
+
+from task import Task, ThreadTaskDispatcher, TaskManager
+
+class HttpClient(asyncore.dispatcher):
+    # Based on an example in Doug Hellmann's PyMOTW:
+    # http://www.doughellmann.com/PyMOTW/asyncore/
+    def __init__(self, url, consumer, finished_callback=None, finished_data=None):
+        self.url = url
+        self.consumer = consumer
+        self.finished_callback = finished_callback
+        self.finished_data = finished_data
+        self.log = logging.getLogger(self.url)
+        self.parsed_url = urlparse.urlparse(url)
+        asyncore.dispatcher.__init__(self)
+        self.write_buffer = 'GET %s HTTP/1.0\r\n\r\n' % self.url
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        address = (self.parsed_url.netloc, 80)
+        self.log.debug('connecting to %s', address)
+        self.connect(address)
+
+    def handle_connect(self):
+        self.log.debug('handle_connect()')
+
+    def handle_close(self):
+        self.log.debug('handle_close()')
+        self.consumer.close()
+        self.close()
+        if self.finished_callback:
+            self.finished_callback(self, self.finished_data)
+
+    def writable(self):
+        is_writable = (len(self.write_buffer) > 0)
+        if is_writable:
+            self.log.debug('writable() -> %s', is_writable)
+        return is_writable
+    
+    def readable(self):
+        self.log.debug('readable() -> True')
+        return True
+
+    def handle_write(self):
+        sent = self.send(self.write_buffer)
+        self.log.debug('handle_write() -> "%s"', self.write_buffer[:sent])
+        self.write_buffer = self.write_buffer[sent:]
+
+    def handle_read(self):
+        data = self.recv(8192)
+        self.log.debug('handle_read() -> %d bytes', len(data))
+        if len(data) > 0:
+            self.consumer.write(data)
+
+class Consumer(object):
+    def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.size = 0
+    
+    def write(self, data):
+        pass
+    
+    def close(self):
+        pass
+
+class PrintConsumer(Consumer):
+    def write(self, data):
+        self.log.debug('absorbing %d bytes', len(data))
+        self.size += len(data)
+
+class FileConsumer(Consumer):
+    def __init__(self, path):
+        Consumer.__init__(self)
+        self.path = path
+        self.temp_path = path + ".part"
+        self.fh = open(self.temp_path, "wb")
+        self.header = ""
+        self.header_complete = False
+        
+    def write(self, data):
+        self.log.debug('saving %d bytes', len(data))
+        if not self.header_complete:
+            self.header += data
+            i = self.header.find("\r\n\r\n")
+            if i < 0:
+                return
+            data = self.header[i+4:]
+            self.header = self.header[:i+2]
+            self.header_complete = True
+        self.fh.write(data)
+        self.size += len(data)
+    
+    def close(self):
+        self.log.debug('saved %d bytes to %s', self.size, self.path)
+        self.fh.close()
+        if os.path.exists(self.path):
+            os.remove(self.path)
+        os.rename(self.temp_path, self.path)
+
+
+class DownloadTask(Task):
+    def __init__(self, url, path, overwrite=False):
+        Task.__init__(self)
+        self.url = url
+        self.path = path
+        self.overwrite = overwrite
+        self.size = 0
+    
+    def _start(self, processor):
+        if os.path.exists(self.path) and not self.overwrite:
+            self.size = os.path.getsize(self.path)
+            processor._finished.put(self)
+        else:
+            consumer = FileConsumer(self.path)
+            client = HttpClient(self.url, consumer, self._finished_callback, processor)
+    
+    def _finished_callback(self, client, processor):
+        self.size = client.consumer.size
+        processor._finished.put(self)
+
+
+class BackgroundHttpDownloader(ThreadTaskDispatcher):
+    def __init__(self):
+        ThreadTaskDispatcher.__init__(self)
+        self._counter = 0
+        self._timeout = 1
+        self._max_simultaneous = 5
+    
+    def can_handle(self, task):
+        return isinstance(task, DownloadTask)
+    
+    def run(self):
+        self.log.debug("starting thread...")
+        while True:
+            if self._want_abort:
+                break
+
+            if asyncore.socket_map:
+                self._counter += 1
+                self.log.debug('counter=%s, # sockets = %d', self._counter, len(asyncore.socket_map))
+                asyncore.loop(timeout=self._timeout, count=1)
+            else:
+                self.log.debug('no clients')
+                time.sleep(1)
+            
+            if len(asyncore.socket_map) < self._max_simultaneous:
+                try:
+                    task = self._queue.get(False)
+                    if task is None:
+                        return
+                    task._start(self)
+                    self.log.debug("found client %s", task.url)
+                except Queue.Empty:
+                    self.log.debug("waiting for clients...")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(name)s: %(message)s',
+                        )
+
+    manager = TaskManager()
+    downloader = BackgroundHttpDownloader()
+    manager.start_processor(downloader)
+    manager.add_task(DownloadTask('http://www.python.org/', "python.html", overwrite=True))
+    manager.add_task(DownloadTask('http://www.doughellmann.com/PyMOTW/contents.html',"pymotw.html"))
+    manager.add_task(DownloadTask('http://docs.python.org/release/2.6.8/_static/py.png', "py.png"))
+    manager.add_task(DownloadTask('http://docs.python.org/release/2.6.8/_static/py.png', "py2.png"))
+    manager.add_task(DownloadTask('http://docs.python.org/release/2.6.8/_static/py.png', "py3.png"))
+    for i in range(10):
+        time.sleep(1)
+        tasks = manager.get_finished()
+        for task in tasks:
+            print 'FINISHED:', task.size, 'bytes for', task.path, 'from', task.url
+        if i == 5:
+            manager.add_task(DownloadTask('http://www.python.org/images/python-logo.gif', "bigpy.gif"))
+            
+    manager.shutdown()
---
+++ dinoteeth/task.py
@@ -0,0 +1,267 @@
+import os, time, logging, threading, multiprocessing, Queue
+
+# Utilities for thread and process based tasks
+
+
+
+# Don't use the logger module in anything using multiprocessing! It can cause
+# deadlocks.  I.e. don't use this:
+#### log = logging.getLogger("dinoteeth.task")
+
+log = multiprocessing.log_to_stderr()
+#log.setLevel(logging.INFO)
+
+
+
+class Task(object):
+    def __init__(self):
+        self.error = None
+        self.exception = None
+    
+    def __str__(self):
+        status = self.get_name()
+        if not self.success():
+            if self.error is not None:
+                status += " Failed: %s" % self.error
+            else:
+                status += " Exception: %s" % self.exception
+        return status
+    
+    def success(self):
+        return self.error is None and self.exception is None
+    
+    def get_name(self):
+        return self.__class__.__name__
+        
+    def _start(self, processor):
+        raise RuntimeError("Abstract method")
+
+class ThreadTask(Task):
+    pass
+
+class ProcessTask(Task):
+    def _start(self, results):
+        raise RuntimeError("Abstract method")
+
+class TestSleepTask(ThreadTask):
+    def __init__(self, num, sleep):
+        Task.__init__(self)
+        self.num = num
+        self.sleep = sleep
+    
+    def get_name(self):
+        return "thread sleep task #%d, delay=%ss" % (self.num, self.sleep)
+        
+    def _start(self, processor):
+        log.debug("%s starting!" % self.get_name())
+        time.sleep(self.sleep)
+
+class TestProcessSleepTask(ProcessTask):
+    def __init__(self, num, sleep):
+        Task.__init__(self)
+        self.num = num
+        self.sleep = sleep
+    
+    def get_name(self):
+        return "process sleep task #%d, delay=%ss" % (self.num, self.sleep)
+        
+    def _start(self, processor):
+        log.debug("%s starting!" % self.get_name())
+        time.sleep(self.sleep)
+
+
+class TaskDispatcher(object):
+    def __init__(self, share_input_queue_with=None):
+        self._want_abort = False
+        if share_input_queue_with is not None:
+            self._queue = share_input_queue_with._queue
+        else:
+            self._queue = Queue.Queue()
+    
+    def set_finish_queue(self, finish_queue):
+        self._finished = finish_queue
+    
+    def start_processing(self):
+        raise RuntimeError("Abstract method")
+    
+    def can_handle(self, task):
+        return False
+    
+    def add_task(self, task):
+        self._queue.put(task)
+
+    def abort(self):
+        # Method for use by main thread to signal an abort
+        self._want_abort = True
+        self._queue.put(None)
+
+class ThreadTaskDispatcher(threading.Thread, TaskDispatcher):
+    def __init__(self, share_input_queue_with=None):
+        threading.Thread.__init__(self)
+        TaskDispatcher.__init__(self, share_input_queue_with)
+        self.log = log
+    
+    def start_processing(self):
+#        self.setDaemon(True)
+        self.start()
+        
+    def can_handle(self, task):
+        return isinstance(task, ThreadTask)
+    
+    def run(self):
+        log.debug("starting thread...")
+        while True:
+            log.debug("%s waiting for tasks..." % self.name)
+            task = self._queue.get(True) # blocking
+            if task is None or self._want_abort:
+                break
+            try:
+                task._start(self)
+            except Exception, e:
+                import traceback
+                task.exception = traceback.format_exc()
+            self._finished.put(task)
+
+def testsleep(dum):
+    print "Starting ..."
+    time.sleep(1)
+    print "Done..."
+
+
+class Worker(multiprocessing.Process):
+    def __init__(self, task_queue, finished_queue):
+        multiprocessing.Process.__init__(self)
+        self._tasks = task_queue
+        self._finished = finished_queue
+        self.start()
+
+    def run(self):
+        while True:
+            log.debug("%s: worker waiting for task" % self.name)
+            task = self._tasks.get() # block to wait for new task
+            log.debug("%s: worker found task %s" % (self.name, str(task)))
+            if task is None:
+                # "poison pill" means shutdown this worker
+                log.debug("%s: poison pill received. Stopping" % self.name)
+                self._finished.put(None)
+                break
+            try:
+                task._start(self)
+            except Exception, e:
+                import traceback
+                task.exception = traceback.format_exc()
+            self._finished.put(task)
+
+class ProcessTaskDispatcher(ThreadTaskDispatcher):
+    def __init__(self, *args, **kwargs):
+        ThreadTaskDispatcher.__init__(self, *args, **kwargs)
+        self._multiprocessing_tasks = multiprocessing.Queue()
+        self._multiprocessing_finished = multiprocessing.Queue()
+        self._worker = Worker(self._multiprocessing_tasks, self._multiprocessing_finished)
+        log.debug("worker %s: status = %s" % (self._worker, self._worker.exitcode))
+        
+    def can_handle(self, task):
+        return isinstance(task, ProcessTask)
+    
+    def run(self):
+        log.debug("%s: starting process task dispatcher thread..." % self.name)
+        while True:
+            log.debug("%s: waiting for tasks..." % self.name)
+            task = self._queue.get(True) # blocking
+            
+            # Send task to worker and wait for it to finish
+            log.debug("%s: sending task '%s' to process %s" % (self.name, task, self._worker))
+            self._multiprocessing_tasks.put(task)
+            task = self._multiprocessing_finished.get(True)
+            
+            # Send results back to main thread
+            self._finished.put(task)
+            
+            if task is None:
+                log.debug("%s: Stopping process %s" % (self.name, self._worker))
+                self._worker.join()
+                log.debug("%s: Exiting dispatcher %s" % (self.name, self.name))
+                break
+
+
+class TaskManager(object):
+    def __init__(self):
+        log = logging.getLogger(self.__class__.__name__)
+        self._finished = Queue.Queue()
+        self.processors = []
+    
+    def find_processor(self, task):
+        for processor in self.processors:
+            if processor.can_handle(task):
+                return processor
+        return None
+            
+    def start_processor(self, processor):
+        log.debug("Adding processor %s" % str(processor))
+        processor.set_finish_queue(self._finished)
+        processor.start_processing()
+        self.processors.append(processor)
+            
+    def add_task(self, task):
+        processor = self.find_processor(task)
+        if processor is not None:
+            log.debug("Adding task %s to %s" % (str(task), str(processor)))
+            processor.add_task(task)
+        else:
+            log.debug("No processor for task %s" % str(task))
+    
+    def get_finished(self):
+        done = []
+        try:
+            while True:
+                task = self._finished.get(False)
+                done.append(task)
+        except Queue.Empty:
+            pass
+        return done
+    
+    def shutdown(self):
+        for processor in self.processors:
+            processor.abort()
+        for processor in self.processors:
+            processor.join()
+
+
+if __name__ == '__main__':
+    manager = TaskManager()
+    processor1 = ThreadTaskDispatcher()
+    manager.start_processor(processor1)
+    processor2 = ThreadTaskDispatcher(processor1)
+    manager.start_processor(processor2)
+    processor3 = ProcessTaskDispatcher()
+    manager.start_processor(processor3)
+    for i in range(3):
+        processor_i = ProcessTaskDispatcher(processor3)
+        manager.start_processor(processor_i)
+    
+    manager.add_task(TestSleepTask(1, 1))
+    manager.add_task(TestSleepTask(2, 1))
+    manager.add_task(TestSleepTask(3, 3))
+    manager.add_task(TestSleepTask(4, 1))
+    manager.add_task(TestSleepTask(5, 1))
+    manager.add_task(TestProcessSleepTask(10, .1))
+    manager.add_task(TestProcessSleepTask(11, .1))
+    manager.add_task(TestProcessSleepTask(12, .3))
+    manager.add_task(TestProcessSleepTask(13, .1))
+    manager.add_task(TestProcessSleepTask(14, .1))
+    for i in range(5):
+        time.sleep(1)
+        tasks = manager.get_finished()
+        for task in tasks:
+            print 'FINISHED:', str(task)
+
+#    manager.add_task(TestProcessSleepTask(6, 1))
+    manager.shutdown()
+    tasks = manager.get_finished()
+    for task in tasks:
+        print 'FINISHED:', str(task)
+    for i in range(5):
+        time.sleep(1)
+        tasks = manager.get_finished()
+        for task in tasks:
+            print 'FINISHED:', str(task)
