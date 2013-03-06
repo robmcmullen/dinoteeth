a485ebade9b34ecafe81ffe72cf458813f434a05
Rob McMullen <robm@users.sourceforge.net>
2013-03-06 14:40:04 -0800

    Removed duplicate entries in Film Series list

--- dinoteeth/settings.py
+++ dinoteeth/settings.py
@@ -66,7 +66,7 @@ credit_map = [
     # Title, attribute, limit_category (one of: 'series', 'movies' or None), converter (to change the data; e.g. change a raw timestamp into a month/year for bucketing), reverse sort flag (True or False/None)
     ("By Date Added", "date_added", None, lambda d: time.strftime("%B %Y", time.localtime(d)), True),
     ("By Genre", "genres", None, None, False),
-    ("By Film Series", "film_series", "movies", None, False),
+    ("By Film Series", "film_series", "movies", lambda d: unicode(d), False),
     ("By Director", "directors", None, None, False),
     ("By Actor", "cast", None, None, False),
     ("By Executive Producer", "executive_producers", "series", None, False),
